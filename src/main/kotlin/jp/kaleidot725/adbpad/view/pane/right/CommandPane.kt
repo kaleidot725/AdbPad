cd410f55300589699c43ea16af64db86a6c65211
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-08-16 22:47:41 +0900

    Fix right panes

--- src/main/kotlin/jp/kaleidot725/adbpad/view/pane/right/CommandPane.kt
+++ src/main/kotlin/jp/kaleidot725/adbpad/view/pane/right/CommandPane.kt
@@ -1,9 +1,15 @@
 package jp.kaleidot725.adbpad.view.page
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun CommandPane() {
-    Text("CommandPane")
+    Box(Modifier.fillMaxSize()) {
+        Text("CommandPane", Modifier.align(Alignment.Center))
+    }
 }
--- src/main/kotlin/jp/kaleidot725/adbpad/view/pane/right/ScreenshotPane.kt
+++ src/main/kotlin/jp/kaleidot725/adbpad/view/pane/right/ScreenshotPane.kt
@@ -1,10 +1,15 @@
 package jp.kaleidot725.adbpad.view.page
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun ScreenShotPane() {
-    Text("ScreenShotPane")
-
+    Box(Modifier.fillMaxSize()) {
+        Text("ScreenShotPane", Modifier.align(Alignment.Center))
+    }
 }
