6a3b37e2d885105c53b0e77b24bb64e16ca3fbf1
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-10-30 11:21:46 +0900

    Rename setting file creator

--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/usecase/input/AddInputTextUseCase.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/usecase/input/AddInputTextUseCase.kt
@@ -1,19 +1,19 @@
 package jp.kaleidot725.adbpad.domain.usecase.input
 
 import jp.kaleidot725.adbpad.domain.model.Setting
-import jp.kaleidot725.adbpad.domain.service.SettingFileService
+import jp.kaleidot725.adbpad.domain.service.SettingFileCreator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class AddInputTextUseCase {
     suspend operator fun invoke(text: String): Boolean {
         return withContext(Dispatchers.IO) {
-            val oldSetting = SettingFileService.load() ?: Setting()
+            val oldSetting = SettingFileCreator.load() ?: Setting()
             if (oldSetting.inputTexts.any { it == text }) return@withContext true
 
             val newInputTexts = oldSetting.inputTexts.toMutableList().apply { add(text) }
             val newSetting = oldSetting.copy(inputTexts = newInputTexts)
-            return@withContext SettingFileService.save(newSetting)
+            return@withContext SettingFileCreator.save(newSetting)
         }
     }
 }
--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/usecase/input/GetInputTextUseCase.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/usecase/input/GetInputTextUseCase.kt
@@ -1,13 +1,13 @@
 package jp.kaleidot725.adbpad.domain.usecase.input
 
-import jp.kaleidot725.adbpad.domain.service.SettingFileService
+import jp.kaleidot725.adbpad.domain.service.SettingFileCreator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class GetInputTextUseCase {
     suspend operator fun invoke(): List<String> {
         return withContext(Dispatchers.IO) {
-            val setting = SettingFileService.load()
+            val setting = SettingFileCreator.load()
             return@withContext setting.inputTexts
         }
     }
