171d65c2f001afaffebca8302fb28ee32aa91a8e
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-11-04 15:13:34 +0900

    Change icon

--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/model/Menu.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/model/Menu.kt
@@ -2,12 +2,12 @@ package jp.kaleidot725.adbpad.domain.model
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DoubleArrow
-import androidx.compose.material.icons.filled.Input
 import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class Menu(val title: String, val icon: ImageVector) {
     object Command : Menu(Language.MENU_COMMAND_TITLE, Icons.Default.DoubleArrow)
-    object InputText : Menu(Language.MENU_INPUT_TEXT_TITLE, Icons.Default.Input)
+    object InputText : Menu(Language.MENU_INPUT_TEXT_TITLE, Icons.Default.Send)
     object Screenshot : Menu(Language.MENU_SCREENSHOT, Icons.Default.PhotoCamera)
 }
