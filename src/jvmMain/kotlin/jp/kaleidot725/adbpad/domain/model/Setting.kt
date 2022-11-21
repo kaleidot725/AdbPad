ebf2c83218ecc96d0505ec0cad49cd4f185c962b
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-11-21 21:34:40 +0900

    Add path settings

--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/model/Setting.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/model/Setting.kt
@@ -4,6 +4,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Setting(
+    val adbPath: String? = null,
+    val androidSdkPath: String? = null,
+    val adbServerPort: Int? = 20010,
     val inputTexts: List<String> = emptyList(),
     val windowSize: WindowSize = WindowSize.DEFAULT
 )
