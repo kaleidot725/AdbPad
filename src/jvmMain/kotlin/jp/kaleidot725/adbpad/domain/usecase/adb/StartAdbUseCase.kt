0dc364161016d699da356670f56da11662392ca7
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-11-24 21:24:45 +0900

    Set start adb parameter

--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/usecase/adb/StartAdbUseCase.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/usecase/adb/StartAdbUseCase.kt
@@ -1,13 +1,22 @@
 package jp.kaleidot725.adbpad.domain.usecase.adb
 
 import com.malinskiy.adam.interactor.StartAdbInteractor
+import jp.kaleidot725.adbpad.domain.repository.SdkPathRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
 
-class StartAdbUseCase {
+class StartAdbUseCase(
+    private val sdkPathRepository: SdkPathRepository
+) {
     suspend operator fun invoke(): Boolean {
         return withContext(Dispatchers.IO) {
-            return@withContext StartAdbInteractor().execute()
+            val sdkPath = sdkPathRepository.get()
+            return@withContext StartAdbInteractor().execute(
+                adbBinary = File(sdkPath.adbDirectory),
+                androidHome = File(sdkPath.androidSdkDirectory),
+                serverPort = sdkPath.adbServerPort
+            )
         }
     }
 }
