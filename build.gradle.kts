2f5642ca13f161650bc91deace862c5558aadba6
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-12-07 21:58:46 +0900

    Update conveyor setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ kotlin {
     sourceSets {
         all {
             dependencies {
+                implementation(kotlin("stdlib-jdk8"))
                 implementation(compose.desktop.currentOs)
                 implementation(compose.material)
                 implementation(compose.materialIconsExtended)
@@ -45,6 +46,7 @@ kotlin {
         }
         val jvmMain by getting {
             dependencies {
+                implementation(kotlin("stdlib-jdk8"))
                 implementation(compose.desktop.currentOs)
                 implementation(compose.material)
                 implementation(compose.materialIconsExtended)
@@ -111,6 +113,16 @@ configurations.all {
     attributes {
         // https://github.com/JetBrains/compose-jb/issues/1404#issuecomment-1146894731
         attribute(Attribute.of("ui", String::class.java), "awt")
+    }
+}
+
+// Force override the Kotlin stdlib version used by Compose to 1.7, as otherwise we can end up with a mix of 1.6 and 1.7 on our classpath.
+dependencies {
+    val v = "1.7.10"
+    for (m in setOf("linuxAmd64", "macAmd64", "macAarch64", "windowsAmd64")) {
+        m("org.jetbrains.kotlin:kotlin-stdlib:$v")
+        m("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$v")
+        m("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$v")
     }
 }
 // endregion
