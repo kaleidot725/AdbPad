362c4a1a74b6e92b65c24f7961cd167fcf5af5e3
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2025-01-06 21:27:40 +0900

    feat: fix bugs

--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/ui/screen/screenshot/component/ScreenshotGallery.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/ui/screen/screenshot/component/ScreenshotGallery.kt
@@ -1,19 +1,28 @@
 package jp.kaleidot725.adbpad.ui.screen.screenshot.component
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.defaultScrollbarStyle
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import jp.kaleidot725.adbpad.domain.model.UserColor
@@ -27,7 +36,9 @@ fun ScreenshotGallery(
     modifier: Modifier = Modifier,
 ) {
     Box(modifier) {
+        val lazyColumnState = rememberLazyListState()
         LazyColumn(
+            state = lazyColumnState,
             contentPadding = PaddingValues(horizontal = 16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             modifier = Modifier.padding(vertical = 16.dp),
@@ -60,5 +71,10 @@ fun ScreenshotGallery(
                 )
             }
         }
+
+        VerticalScrollbar(
+            modifier = Modifier.align(Alignment.CenterEnd).width(8.dp).fillMaxHeight(),
+            adapter = rememberScrollbarAdapter(scrollState = lazyColumnState)
+        )
     }
 }
