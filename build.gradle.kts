07e2fedb24a2e35f5f2a12c21ac874fd038d1403
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2025-02-11 13:18:55 +0900

    chore: update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "jp.kaleidot725"
-version = "1.5.0"
+version = "1.5.1"
 
 kotlin {
     jvm()
--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/model/language/resources/StringResources.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/model/language/resources/StringResources.kt
@@ -1,6 +1,6 @@
 package jp.kaleidot725.adbpad.domain.model.language.resources
 
-val APP_VERSION = "v1.5.0"
+val APP_VERSION = "v1.5.1"
 
 interface StringResources {
     val windowTitle: String
