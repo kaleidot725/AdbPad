4cdefdabdd15201c68857a437fa52be6f6e26580
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2025-04-26 13:09:08 +0900

    chore: update version

--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/model/language/resources/StringResources.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/model/language/resources/StringResources.kt
@@ -1,6 +1,6 @@
 package jp.kaleidot725.adbpad.domain.model.language.resources
 
-const val APP_VERSION = "v2.4.0"
+const val APP_VERSION = "v2.5.0"
 
 interface StringResources {
     val windowTitle: String
