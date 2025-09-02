e091fa11ae5511260eca1a7a0e5c4d5f7f9b87c5
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2025-09-03 08:59:49 +0900

    fix: build error

--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/ui/screen/device/section/ScrcpyOptionsSection.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/ui/screen/device/section/ScrcpyOptionsSection.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.matchParentSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
