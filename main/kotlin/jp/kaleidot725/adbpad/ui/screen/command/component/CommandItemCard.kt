32afe94299e94dd7c331e3b976a940a7f8d4f5ed
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2026-01-10 21:01:39 +0900

    style: format source code

--- main/kotlin/jp/kaleidot725/adbpad/ui/screen/command/component/CommandItemCard.kt
+++ main/kotlin/jp/kaleidot725/adbpad/ui/screen/command/component/CommandItemCard.kt
@@ -1,7 +1,6 @@
 package jp.kaleidot725.adbpad.ui.screen.command.component
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
--- main/kotlin/jp/kaleidot725/adbpad/ui/screen/command/component/CommandItemList.kt
+++ main/kotlin/jp/kaleidot725/adbpad/ui/screen/command/component/CommandItemList.kt
@@ -1,6 +1,5 @@
 package jp.kaleidot725.adbpad.ui.screen.command.component
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
